d5bd51df15876f1dd2e922cbdf146283ec1acd91
janhchaeyeon <codus_13@naver.com>
2024-12-17 21:35:09 +0900

    Add files via upload

--- 수업.java
+++ 수업.java
@@ -1,53 +1,91 @@
-package java_study_1210;
+package java_study_1217;
 
+import java.util.Scanner;
 
-//클래스 이름 : 수업
-//클래스이름은 앞에 대문자로 작성한다.
+/*
+ * 계산기 클래스는 더이상 수정 불가능 (내장클래스는 실제 수정이 불가능함)
+ * Scanner 클래스 -> 내장 클래스(자바를 설치했을 때 이미 자바 안에 있는 클래스들)
+ * Random 클래스
+ */
+
+class 계산기{
+	
+	//파라미터값을 각 더해서 리턴하는 함수 sum
+	public int sum(int x, int y) {
+		return x + y;
+		
+	}
+	
+
+}
+//눈치빠른 개발자는 계산기클래스를 그대로 복사해서 하나 더 만든다!
+
+class 계산기2{
+	
+	//파라미터값을 각 더해서 리턴하는 함수 sum
+	public int sum(int x, int y) {
+		return x + y;
+	}
+	public int minus(int x, int y) {
+		return x - y;
+	}
+	
+}
+
+//extends*** : 상속
+//상속을 이용해 '계산기' 클래스 기능을 물려받을 수 있음
+//'계산기3'(자식클래스 == subClass) 은 
+//'계산기'(부모클래스 == superClass) 에 있는 필드변수와 메소드를 사용할 수 있다.
+
+class 계산기3 extends 계산기{
+	
+	//부모클래스에 있는 메소드 가져오기
+	//@Override : 오버라이딩 (부모클래스 메소드 *******'재정의')
+	
+	/*
+	 * 오버로딩 : 메소드 이름 중복 허용하기(파라미터 타입 다르게 지정)
+	 * 오버라이딩 : 부모클래스에 정의된 메소드를 자식클래스에서 호출해서 메소드 '재정의'
+	 * 
+	 */
+	@Override
+	public int sum(int x, int y) {
+		// TODO Auto-generated method stub
+		return super.sum(x, y);
+	}
+	
+	
+	public int minus(int x, int y) {
+		return x - y; //상속을 통해 더하기, 마이너스 해결
+
+}
+	
+	
+
+}
+
 public class 수업 {
 
-	int x = 10; //필드변수(전역변수)
-	
-	public 수업() { //생성자 
-		
-	}
-	public int getx() {//메소드
-		return x;
-	}
-	
-	
 	public static void main(String[] args) {
-		//객체지향 : 유지보수하기 쉽게 코드를 나눈다
-		//지금까지 main함수에 개발을 진행함
-		//근데 목요일부터는 새로운 함수에 코드를 작성할 예정
+		
 
+		계산기 계산기1 = new 계산기();
+		int result = 계산기1.sum(10,10);
+		System.out.println("결과 : " + result);
 		
-		//붕어빵 클래스를 호출(파일 불러오기)
-		//new : 클래스 호출
-		//x : 붕어빵 클래스 변수이름
-		붕어빵 x = new 붕어빵();
-		x.붕어빵종류 = "팥"; //붕어빵 클래스에 있는 붕어빵종류 변수 사용
-		x.붕어빵가격 = 500; //붕어빵 클래스에 있는 붕어빵가격 변수 사용
-		// . : 접근하다
-		System.out.println("붕어빵 종류 : " + x.붕어빵종류);
-		System.out.println("붕어빵 가격 : " + x.붕어빵가격);
 		
+		//빼기와 더하기 기능이 있는 클래스가 필요
+		
+		//내 생각 : 계산기 클래스를 불러온 다	음.. x 위에서 복사본 하나 만들기. 대신 더하기가 중복됨.
 		
 		/*
-		 * 메소드안에 작성된 변수는 '지역변수' 라고 함
-		 * 지역변수는 선언과 동시에 값을 초기화(must) 해야합니다.
-		 * 
+		 * 자바는 무료 오픈소스이기 때문에 내장클래스를 확인할 수 있음
 		 */
 		
-		int num = 0; //0값 초기화하기
-		System.out.println(num); // 에러 발생 - int num;만 작성했을 때 에러.
 		
+		Scanner scan = new Scanner(System.in); //ctrl+shift+o (자동 임포트)
 		
-		붕어빵 y = new 붕어빵(); //붕어빵 클래스 재호출(기존 호출 무시. 새롭게 호출)
-		System.out.println("붕어빵 종류 : " + y.붕어빵종류);
-		System.out.println("붕어빵 가격 : " + y.붕어빵가격);
-	
-		
+		계산기3 계산기3 = new 계산기3();
+		계산기3.sum(10, 20); //자식클래스가 부모클래스에 있는 메소드 사용 가능
 	}
-	
 
 }
---
+++ 수업2.java
@@ -0,0 +1,114 @@
+package java_study_1217;
+
+class Food{
+	
+	String name;
+	int calories;
+	
+	
+	public Food(String name, int calories) {
+		this.name = name;
+		this.calories = calories;
+		
+	}
+	
+	public void eat() {
+		System.out.println("Eating : " + this.name);
+		
+	}
+	
+	public int getCalories() {
+		return this.calories;
+	}
+}//Food 클래스 끝
+
+
+class Pizza extends Food{//피자클래스
+	
+	String[] toppings; //토핑 (문자열 배열)
+	
+	public Pizza(String name, int calories, String[] toppings) {
+		super(name, calories); //super : 부모클래스 생성자 호출
+		this.toppings = toppings;
+		
+		
+	}
+	@Override
+	public void eat() {//부모클래스에 있는 eat메소드 재정의
+		
+		System.out.println("Eating a slice of " + super.name + " pizza");
+		
+	}
+	
+	public void showToppings() {
+			System.out.println("Toppings : ");
+			for(String s : toppings) {
+			System.out.println(s + ", ");
+		}
+			System.out.println("");
+	}
+	
+	
+}//끝
+
+class Sushi extends Food{//스시클래스
+	String fishType;
+	
+	public Sushi(String name, int calories, String fishType) {
+		super(name, calories); // 부모 클래스 생성자 호출
+		this.fishType = fishType; //필드 변수 대입
+	}
+	
+	@Override
+	public void eat() {
+	
+		System.out.println("Eating"+super.name+"sushi");
+	}
+	
+	public void showFishType() {
+		System.out.println("FishType : "+this.fishType);
+	}
+	
+}//끝
+
+
+public class 수업2 {
+	
+
+	public static void main(String[] args) {
+		
+		
+		Food food = new Food("Food", 100);//부모클래스
+		
+		String[] toppings = {"Tomato", "Mozzarella", "Basil"};
+		Pizza margherita = new Pizza("margherita", 300, toppings);
+		
+		Sushi salmonRoll = new Sushi("salmon Roll", 250 ,"Salmon");
+		
+		food.eat(); //푸드 클래스 eat 호출
+		margherita.eat(); //피자 클래스 eat(재정의한 메소드) 호출
+		salmonRoll.eat(); //스시 클래스 eat(재정의한 메소드) 호출
+		
+		
+		margherita.showToppings();
+		salmonRoll.showFishType();
+		
+		
+		/* 코드설명
+		 * 
+		 * Food 클래스 : 기본 음식 클래스, 모든 음식의 공통 속성인
+		 * name과 calories 그리고 eat() 메소드를 가진다.
+		 * 
+		 * 
+		 * Pizza와 Sushi 클래스 : Food를 상속받아 eat 메소드를 오버라이딩
+		 * 또한 Pizza 클래스에는 토핑을 보여주는 showToppings() 메소드 구현
+		 * Sushi 클래스에는 생선종류를 보여주는 showFishType() 메소드 구현
+		 * 
+		 * 핵심 : Pizza와 Sushi 클래스는 Food 클래스의 속성과 메소드를 상속받아
+		 * 재사용할 수 있습니다.
+		 * 
+		 */
+
+	}
+
+}
